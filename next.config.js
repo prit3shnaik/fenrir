8d1ee156321ac3f6edc4e31477dcb02eedbb8461
Pritesh Naik <97189178+prit3shnaik@users.noreply.github.com>
2026-04-25 00:44:32 +0530

    Add ESLint and TypeScript configuration options

--- next.config.js
+++ next.config.js
@@ -5,5 +5,8 @@ const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: false },
 }
+
 module.exports = nextConfig
