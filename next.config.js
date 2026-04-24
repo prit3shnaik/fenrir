d50211e6fbe824305161815311e5769f784f42b9
Pritesh Naik <97189178+prit3shnaik@users.noreply.github.com>
2026-04-25 00:55:02 +0530

    Remove basePath and assetPrefix from next.config.js
    Removed basePath and assetPrefix settings from the configuration.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/fenrir',
-  assetPrefix: '/fenrir/',
   output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
