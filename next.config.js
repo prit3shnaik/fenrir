3408713dc0619cb5c20af8c1be0f928021ea8e57
Pritesh Naik <97189178+prit3shnaik@users.noreply.github.com>
2026-04-25 01:01:15 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/fenrir',
+  assetPrefix: '/fenrir/',
   output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
